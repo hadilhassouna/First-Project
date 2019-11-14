8499e0522b03e4d3ff8396f88eab2305c3b9a6d5
YaraSalhi <yara.salhi@outlook.sa>
2019-11-14 05:00:55 -0800

    accept

--- models/item.js
+++ models/item.js
@@ -7,71 +7,97 @@ const Items =  new Schema ({
     time: String ,
     userName:String,
     userId:Number,
+    userRelatedWorks:String,
+    worksId:Number,
     views:Number,
     progilePicReviews:String,
-    date:Date,
-    projectId:String,
-    projectInfo:String,
+    date:Date
+
+});
+
+
+const userinfo= new Schema({
+    name : String,
+    location : String
+})
+
+
+const Reviewers =  new Schema ({
+    profilePic: String,
+    time: String ,
+    userName:String,
+    comment:String
+});
+const img =  new Schema ({
+   
+    itemid:Number,
+    url:String,
     imgInfo:String
 });
 
-const reviews =  new Schema ({
-    profilePic:String,
-    name:  String ,
-    time: Number ,
-    userName:String,
-    comment: String
-
-});
-const img =  new Schema ({
-    userid:Number,
-    projectid:Number,
+const others =  new Schema ({
+   
     itemid:Number,
     url:String,
-    imgInfo:String,
-    projectInfo:String,
     projectName:String
 });
 
-
-
+const Item = mongoose.model("item", Items);
 const Imgs = mongoose.model("img", img);
-const review = mongoose.model("review", reviews);
+const Others = mongoose.model("others", others);
+const rev = mongoose.model("reviewers", Reviewers);
+const Userinfo = mongoose.model("userinfo", userinfo);
 
-
-const review1 =  new review ({
-
-    profilePic:"String",
-    name:  "String" ,
-    time: 2 ,
-    userName:"String",
-    comment: "String"
+const others1 =  new Others ({
+   
+    itemid:1,
+    url:"String",
+    projectName:"String"
 });
 
 const img1 =  new Imgs ({
-    userid:1,
-    projectid:1,
+
     itemid:1,
     url:"anuthing",
-    imgInfo:"hhh",
-    projectInfo:"data about project",
-    projectName: "projectName"
+    imgInfo:"hhh"
 });
 
+const userinfo1 = new  Userinfo({
+    name: "oladog",
+    location : "Jordan"
+})
 
-// var savingToDb = new Item({
-//     image:"l",
-//     imgInfo:"ola",
-//     userName:"olathedog",
-//     userId:45445,
-//     userRelatedWorks:"Something",
-//     worksId:33455,
-//     views:33333,
-//     progilePicReviews:"Something"
+const  reviewers1 = new  rev({
+    profilePic: "mkjk",
+    time: "ll",
+    userName:"samar",
+    comment:"nhnlk"
+})
+
+var savingToDb = new Item({
+    image:"l",
+    imgInfo:"ola",
+    userName:"olathedog",
+    userId:45445,
+    userRelatedWorks:"Something",
+    worksId:33455,
+    views:33333,
+    progilePicReviews:"Something"
 
     
 
-// });
+});
+
+// others1.save((error,result)=>{
+//     if(error){
+//         console.log("other error",error
+//         )
+//     }
+//     else{
+//     console.log("other done")
+//     }
+//     });
+    
 
 // img1.save((error,result)=>{
 // if(error){
@@ -79,19 +105,10 @@ const img1 =  new Imgs ({
 //     )
 // }
 // else{
-// console.log("img don")
+// console.log("img done")
 // }
 // });
 
-// review1.save((error,result)=>{
-//     if(error){
-//         console.log("review error",error
-//         )
-//     }
-//     else{
-//     console.log("review don")
-//     }
-//     });
 
 // savingToDb.save((error,result)=>{
 // if(error){
@@ -102,10 +119,29 @@ const img1 =  new Imgs ({
 
 // });
 
+// userinfo1.save((error,result)=>{
+// if(error){
+//     console.log("user error",error
+//     )
+// }
+// else{
+// console.log("user done")
+// }
+// });
+
+// reviewers1.save((error,result)=>{
+//     if(error){
+//         console.log("revs error",error
+//         )
+//     }
+//     else{
+//     console.log("revs done")
+//     }
+//     });
+    
 
 module.exports.Items=Items;
 module.exports.Imgs=Imgs;
-module.exports.reviews=reviews;
-
-
-
+module.exports.Userinfo=Userinfo;
+module.exports.rev=rev;
+module.exports.Others=Others;
